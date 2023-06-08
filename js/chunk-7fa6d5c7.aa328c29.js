187a396933bafec86cc58df93faa978d50f6b74f
alx110 <alx110@gmail.com>
2023-06-08 07:33:03 +0000

    feat: init

---
+++ js/chunk-7fa6d5c7.aa328c29.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-7fa6d5c7"],{"72f2":function(e,n,t){"use strict";t.r(n);var c=t("7a23");function o(e,n,t,o,a,r){const d=Object(c["resolveComponent"])("router-view");return Object(c["openBlock"])(),Object(c["createBlock"])(d)}var a={name:"vip",data(){return{}}},r=(t("db24"),t("d959")),d=t.n(r);const u=d()(a,[["render",o],["__scopeId","data-v-5c76e070"]]);n["default"]=u},"9ade":function(e,n,t){},db24:function(e,n,t){"use strict";t("9ade")}}]);
+//# sourceMappingURL=chunk-7fa6d5c7.aa328c29.js.map
